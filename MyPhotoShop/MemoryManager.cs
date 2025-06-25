3cf24d5ca46aeffd31414de3adc1762e48c14285
Nicolas VAREILLE <nicolasvareille.gdl@gmail.com>
2025-06-25 15:31:39 +0200

    Ajout du free

---
+++ MyPhotoShop/MemoryManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPhotoShop;
+
+public static unsafe class MemoryManager
+{
+    [UnmanagedCallersOnly(EntryPoint = "Free")]
+    public static void Free(void *ptr)
+    {
+        NativeMemory.Free(ptr);
+        Console.WriteLine("Memory freed");
+    }
+}
--- MyPhotoShop/Runtime.js
+++ MyPhotoShop/Runtime.js
@@ -1,41 +1,50 @@
 var Module = 
 {
-        onRuntimeInitialized: () => {
-            console.log("Chargé ma GL");
+    Canvas: null,
+    Ctx: null,
+    onRuntimeInitialized: () => {
+        console.log("Chargé ma GL");
+    
+        document.getElementById("LoadFile").addEventListener("change", Module.LoadFile);
+        document.getElementById("grayscale").addEventListener("click", Module.Grayscale);
+        document.getElementById("canvas").addEventListener("mousedown", Module.HandleMousePosition); 
+    },
+    LoadFile: async (e) =>
+    {
+        const canvas = document.getElementById("canvas")
+        const ctx = canvas.getContext("2d");
+        const img = new Image();
+        img.src = URL.createObjectURL(e.target.files[0]);
+        await img.decode();
+
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.drawImage(img, 0, 0);
+    },
+    Grayscale: () =>
+    {
+        console.log("Noir & blanc");
+
+        const canvas = document.getElementById("canvas")
+        const ctx = canvas.getContext("2d");
+
+        let buffer = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        let ptr = _malloc(canvas.width * canvas.height * 4);
+
+        HEAPU8.set(buffer.data, ptr);
+
+        _Grayscale(ptr, canvas.width * canvas.height * 4);
+
+        let data = HEAPU8.subarray(ptr, ptr + canvas.width * canvas.height * 4);
         
-            document.getElementById("LoadFile").addEventListener("change", Module.LoadFile);
-            document.getElementById("grayscale").addEventListener("click", Module.Grayscale);
-        },
-        LoadFile: async (e) =>
-        {
-            const canvas = document.getElementById("canvas")
-            const ctx = canvas.getContext("2d");
-            const img = new Image();
-            img.src = URL.createObjectURL(e.target.files[0]);
-            await img.decode();
+        buffer.data.set(data);
+        ctx.putImageData(buffer, 0, 0);
 
-            canvas.width = img.width;
-            canvas.height = img.height;
-            ctx.drawImage(img, 0, 0);
-        },
-        Grayscale: () =>
-        {
-            console.log("Noir & blanc");
-
-            const canvas = document.getElementById("canvas")
-            const ctx = canvas.getContext("2d");
-
-            let buffer = ctx.getImageData(0, 0, canvas.width, canvas.height);
-            let ptr = _malloc(canvas.width * canvas.height * 4);
-
-            HEAPU8.set(buffer.data, ptr);
-
-            _Grayscale(ptr, canvas.width * canvas.height * 4);
-
-            let data = HEAPU8.subarray(ptr, ptr + canvas.width * canvas.height * 4);
-            
-            buffer.data.set(data);
-            ctx.putImageData(buffer, 0, 0);
-        }
+        _Free(ptr);
+    },
+    HandleMousePosition: (e) =>
+    {
+        getMousePosition(canvasElem, e);
+    }
 };
 
